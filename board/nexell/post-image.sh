278956fb6b3f680febd27a13b0bbf2caeff796b7
DeokJin Lee <truevirtue@nexell.co.kr>
2018-11-30 22:35:37 +0900

    buildroot: nexell: modify "/etc/profile" to set tslib configs
    This patch modify the "/etc/profile" to set tslib configs.
    
    Adds:
    	export TSLIB_TSEVENTTYPE=INPUT
    	export TSLIB_TSDEVICE=/dev/input/event1
    	export TSLIB_CALIBFILE=/etc/pointercal
    	export TSLIB_CONFFILE=/etc/ts.conf
    	export TSLIB_PLUGINDIR=/usr/lib/ts
    	export TSLIB_FBDEVICE=/dev/fb0
    
    Change-Id: I20da8ab81706de146bcc3d01a32ac12c627adf61
    Signed-off-by: DeokJin Lee <truevirtue@nexell.co.kr>

--- board/nexell/post-image.sh
+++ board/nexell/post-image.sh
@@ -8,3 +8,7 @@ SKELETON_FS_DIR=board/nexell/fs
 if grep -Eq "^BR2_PACKAGE_ANDROID_TOOLS_ADBD=y$" ${BR2_CONFIG}; then
 	cp $SKELETON_FS_DIR/usr/bin/start_adb.sh ${TARGET_DIR}/usr/bin
 fi
+
+if grep -Eq "^BR2_PACKAGE_TSLIB=y$" ${BR2_CONFIG}; then
+	cp $SKELETON_FS_DIR/etc/profile ${TARGET_DIR}/etc/profile
+fi
