2b214c79c1a8a28ecb099e52cf606f7f608707b9
jhkim <jhkim@nexell.co.kr>
2019-01-09 19:55:05 +0900

    buildroot: nexell: change start adbd function and add stop adbd.sh
    This patch change start adbd function and add stop_adbd.sh
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I5d0e6bec23991ef0351179bbbc8cc347171385c6
    Reviewed-on: https://review.gerrithub.io/c/439682

--- board/nexell/post-image.sh
+++ board/nexell/post-image.sh
@@ -6,7 +6,10 @@ set -e
 SKELETON_FS_DIR=board/nexell/fs
 
 if grep -Eq "^BR2_PACKAGE_ANDROID_TOOLS_ADBD=y$" ${BR2_CONFIG}; then
-	cp $SKELETON_FS_DIR/usr/bin/start_adb.sh ${TARGET_DIR}/usr/bin
+	cp $SKELETON_FS_DIR/usr/bin/start_adbd.sh ${TARGET_DIR}/usr/bin
+	cp $SKELETON_FS_DIR/usr/bin/stop_adbd.sh ${TARGET_DIR}/usr/bin
+	chmod 755 ${TARGET_DIR}/usr/bin/start_adbd.sh
+	chmod 755 ${TARGET_DIR}/usr/bin/stop_adbd.sh
 fi
 
 if grep -Eq "^BR2_PACKAGE_TSLIB=y$" ${BR2_CONFIG}; then
