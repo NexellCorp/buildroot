fa84f3a45fcf992faa6653d812678e316af6ea5b
jhkim <jhkim@nexell.co.kr>
2019-04-24 17:21:03 +0900

    package/nexell: Add disk_test application
    This patch add dist_test application to Nexell packages
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I5d6ac6bab84b4b457c3312d9dd083d38cef2cfec

---
+++ package/nexell/disk_test/disk_test.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# disk_test
+#
+################################################################################
+
+DISK_TEST_TARGET_NAME = disk_test
+DISK_TEST_VERSION = 0.0.1
+DISK_TEST_SITE_METHOD = local
+
+ifndef BR2_PACKAGE_DISK_TEST_LOCAL_PATH
+DISK_TEST_SITE = ../library/disk_test
+else
+DISK_TEST_SITE = $(BR2_PACKAGE_DISK_TEST_LOCAL_PATH)
+endif
+
+# autotools, so we have to do it manually instead of
+# setting DISK_TEST_AUTORECONF = YES
+define DISK_TEST_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+
+DISK_TEST_PRE_CONFIGURE_HOOKS = DISK_TEST_RUN_AUTOGEN
+
+$(eval $(autotools-package))
