a1b8c1bc35576bcc23f3d9975ec71f8590342e94
jhkim <jhkim@nexell.co.kr>
2019-04-24 13:24:05 +0900

    package/nexell: Adds nexell packages
    This patch adds nexell packages
    
    kernel-headers:
    	- copy kernel header file to tool chain (ex> nexell_drm.h)
    
    library:
    	- nx-drm-allocator
    	- nx-v4l2
    	- nx-video-api
    	- libdirectfb-nexell
    
    apps:
    	- hwio
    	- i2c_test
    	- nx_video_api_test
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: Ia13a1e9ada61b7dbc665e2a7dd0b7c7b34189c49

---
+++ package/nexell/libdirectfb-nexell/libdirectfb-nexell.mk
@@ -0,0 +1,43 @@
+################################################################################
+#
+# libdirectfb-nexell
+#
+################################################################################
+
+LIBDIRECTFB_NEXELL_VERSION = 0.0.1
+LIBDIRECTFB_NEXELL_SITE_METHOD = local
+LIBDIRECTFB_NEXELL_INSTALL_STAGING = YES
+
+ifndef BR2_PACKAGE_LIBDIRECTFB_NEXELL_LOCAL_PATH
+LIBDIRECTFB_NEXELL_SITE = ../library/libdirectfb-nexell
+else
+LIBDIRECTFB_NEXELL_SITE = $(BR2_PACKAGE_LIBDIRECTFB_NEXELL_LOCAL_PATH)
+endif 
+
+LIBDIRECTFB_NEXELL_DEPENDENCIES = libdrm directfb
+
+# autotools, so we have to do it manually instead of
+# setting LIBDIRECTFB_NEXELL_AUTORECONF = YES
+define LIBDIRECTFB_NEXELL_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh;
+	cd $(@D) && make distclean;
+endef
+
+LIBDIRECTFB_NEXELL_PRE_CONFIGURE_HOOKS = LIBDIRECTFB_NEXELL_RUN_AUTOGEN
+
+LIBDIRECTFB_NEXELL_CONF_OPTS = --prefix=$(STAGING_DIR)/usr
+
+define LIBDIRECTFB_NEXELL_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) \
+		prefix=/usr MODULEDIR=$(STAGING_DIR)/usr/lib/directfb-1.7-7 \
+		-C $(@D) install
+endef
+
+define LIBDIRECTFB_NEXELL_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE)  \
+		prefix=/usr MODULEDIR=$(TARGET_DIR)/usr/lib/directfb-1.7-7 \
+		-C $(@D) install
+endef
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
---
+++ package/nexell/nx-video-api/nx-video-api.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# nx-video-api
+#
+################################################################################
+
+NX_VIDEO_API_VERSION = 0.0.1
+NX_VIDEO_API_SITE_METHOD = local
+NX_VIDEO_API_INSTALL_STAGING = YES
+
+ifndef BR2_PACKAGE_NX_VIDEO_API_LOCAL_PATH
+NX_VIDEO_API_SITE = ../library/nx-video-api
+else
+NX_VIDEO_API_SITE = $(BR2_PACKAGE_NX_VIDEO_API_LOCAL_PATH)
+endif 
+ 
+NX_VIDEO_API_DEPENDENCIES = libdrm
+
+# autotools, so we have to do it manually instead of
+# setting NX_VIDEO_API_AUTORECONF = YES
+define NX_VIDEO_API_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh;
+	cd $(@D) && make distclean;
+	cd $(@D) && patch -p1 < $(TOPDIR)/$(NX_VIDEO_API_PKGDIR)0001-fix-gem-struct-name.patch;
+endef
+
+NX_VIDEO_API_PRE_CONFIGURE_HOOKS = NX_VIDEO_API_RUN_AUTOGEN
+
+NX_VIDEO_API_CONF_OPTS = --prefix=$(STAGING_DIR)/usr
+
+define NX_VIDEO_API_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) prefix=/usr DESTDIR=$(STAGING_DIR) \
+		-C $(@D) install
+endef
+
+define NX_VIDEO_API_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) prefix=/usr DESTDIR=$(TARGET_DIR) \
+		-C $(@D) install
+endef
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
