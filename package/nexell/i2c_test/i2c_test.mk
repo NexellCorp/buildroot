5b1a74cbb58012346e5ed357d0a05b0d34b8f212
jhkim <jhkim@nexell.co.kr>
2019-04-24 13:24:05 +0900

    package/nexell: Adds nexell packages
    This patch adds nexell packages
    
    kernel-headers:
    	- copy kernel header file to tool chain (ex> nexell_drm.h)
    
    library:
    	- nx-drm-allocator
    	- nx-v4l2
    	- nx-video-api
    	- libdirectfb-nexell
    
    apps:
    	- hwio
    	- i2c_test
    	- nx_video_api_test
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: Ia13a1e9ada61b7dbc665e2a7dd0b7c7b34189c49

---
+++ package/nexell/i2c_test/i2c_test.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# i2c_test
+#
+################################################################################
+
+I2C_TEST_TARGET_NAME = i2c_test
+I2C_TEST_VERSION = 0.0.1
+I2C_TEST_SITE_METHOD = local
+
+ifndef BR2_PACKAGE_I2C_TEST_LOCAL_PATH
+I2C_TEST_SITE = ../library/i2c_test
+else
+I2C_TEST_SITE = $(BR2_PACKAGE_I2C_TEST_LOCAL_PATH)
+endif
+
+# autotools, so we have to do it manually instead of
+# setting I2C_TEST_AUTORECONF = YES
+define I2C_TEST_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+
+I2C_TEST_PRE_CONFIGURE_HOOKS = I2C_TEST_RUN_AUTOGEN
+
+$(eval $(autotools-package))
