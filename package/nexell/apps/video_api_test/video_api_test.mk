bbd85fc5311925b7d2ea05ef4dd10d1161cd58a2
SeongO Park <ray@nexell.co.kr>
2020-01-30 18:34:44 +0900

    dev_lvcc: add and remove applications
    This patch includes the followings.
      1. add applications for video_api_test.
        - nx-video-api
    	- nx-v4l2
    	- nx-drm-allocator
      2. remove useless old libraries.
        - lib_nx_video_api
    	- lib_nx_video_allocator
      3. modify build root config for the above.
        - configs/nxp3220_asv_bus_defconfig
    	- package/nexell/Config.in
    
    Signed-off-by: SeongO Park <ray@nexell.co.kr>
    Change-Id: I473adaf05a75a02c063d55796dcd0b1b54eca827

---
+++ package/nexell/apps/video_api_test/video_api_test.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# VIDEO_API_TEST
+#
+################################################################################
+
+VIDEO_API_TEST_VERSION = 0.0.1
+VIDEO_API_TEST_SITE_METHOD = local
+VIDEO_API_TEST_TARGET_NAME = video_api_test
+
+VIDEO_API_TEST_SITE = package/nexell/apps/video_api_test
+VIDEO_API_TEST_DEPENDENCIES = ffmpeg nx-v4l2 nx-drm-allocator
+
+# autotools, so we have to do it manually instead of
+# setting VIDEO_API_TEST_AUTORECONF = YES
+define VIDEO_API_TEST_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+	cd $(@D) && make distclean;
+endef
+
+VIDEO_API_TEST_PRE_CONFIGURE_HOOKS = VIDEO_API_TEST_RUN_AUTOGEN
+
+VIDEO_API_TEST_CONF_OPTS = --prefix=$(STAGING_DIR)/usr
+
+define VIDEO_API_TEST_INSTALL_STAGING_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) prefix=/usr DESTDIR=$(STAGING_DIR) \
+		-C $(@D) install
+endef
+
+define VIDEO_API_TEST_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) prefix=/usr DESTDIR=$(TARGET_DIR) \
+		-C $(@D) install
+endef
+
+$(eval $(autotools-package))
