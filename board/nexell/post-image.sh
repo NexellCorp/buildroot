e684e77a95410819f4531b9f53d4fc29fa27313a
jhkim <jhkim@nexell.co.kr>
2020-02-20 10:40:00 +0900

    lmbench: intall bw_mem.sh to test memory bandwith
    This patch intall bw_mem.sh to test memory bandwith
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I1a3b9895a8fd7f3277101dacf2def1cef5e682cc

--- board/nexell/post-image.sh
+++ board/nexell/post-image.sh
@@ -6,21 +6,26 @@ set -e
 SKELETON_FS_DIR=board/nexell/fs
 
 if grep -Eq "^BR2_PACKAGE_ANDROID_TOOLS_ADBD=y$" ${BR2_CONFIG}; then
-	cp $SKELETON_FS_DIR/usr/bin/start_adbd.sh ${TARGET_DIR}/usr/bin
-	cp $SKELETON_FS_DIR/usr/bin/stop_adbd.sh ${TARGET_DIR}/usr/bin
-	chmod 755 ${TARGET_DIR}/usr/bin/start_adbd.sh
-	chmod 755 ${TARGET_DIR}/usr/bin/stop_adbd.sh
+	install -m 0755 $SKELETON_FS_DIR/usr/bin/start_adbd.sh \
+			${TARGET_DIR}/usr/bin/start_adbd.sh
+	install -m 0755 $SKELETON_FS_DIR/usr/bin/stop_adbd.sh \
+			${TARGET_DIR}/usr/bin/stop_adbd.sh
 fi
 
 if grep -Eq "^BR2_PACKAGE_TSLIB=y$" ${BR2_CONFIG}; then
-	cp $SKELETON_FS_DIR/etc/profile ${TARGET_DIR}/etc/profile
+	install $SKELETON_FS_DIR/etc/profile ${TARGET_DIR}/etc/profile
 fi
 
 if grep -Eq "^BR2_PACKAGE_DIRECTFB=y$" ${BR2_CONFIG}; then
-	cp $SKELETON_FS_DIR/etc/directfbrc ${TARGET_DIR}/etc/directfbrc
+	install $SKELETON_FS_DIR/etc/directfbrc ${TARGET_DIR}/etc/directfbrc
 fi
 
 if grep -Eq "^BR2_PACKAGE_MEMTESTER=y$" ${BR2_CONFIG}; then
-	cp $SKELETON_FS_DIR/usr/bin/memtest.sh ${TARGET_DIR}/usr/bin
-	chmod 755 ${TARGET_DIR}/usr/bin/memtest.sh
+	install -m 0755 $SKELETON_FS_DIR/usr/bin/memtest.sh \
+			${TARGET_DIR}/usr/bin/memtest.sh
+fi
+
+if grep -Eq "^BR2_PACKAGE_LMBENCH=y$" ${BR2_CONFIG}; then
+	install -m 0755 $SKELETON_FS_DIR/usr/bin/bw_mem.sh \
+			${TARGET_DIR}/usr/bin/bw_mem.sh
 fi
