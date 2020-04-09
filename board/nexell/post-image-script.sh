20917603a8a52e0e7e5b52b747646efb9d2ab31c
jhkim <jhkim@nexell.co.kr>
2020-04-09 14:36:27 +0900

    buildroot: nexell: support systemd autologin
    This patch support systemd autologin
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I65fb1b354c8c6abe23e1fa1dba915cf49643d584
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/buildroot/+/489198

---
+++ board/nexell/post-image-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source board/nexell/post-image.sh
+
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+	if ! grep -Eq "^BR2_INIT_SYSTEMD=y$" ${BR2_CONFIG}; then
+		# set autologin
+		cp ${SKELETON_FS_DIR}/etc/inittab ${TARGET_DIR}/etc/inittab
+	fi
+fi
+
+if grep -Eq "^BR2_TARGET_ROOTFS_CPIO=y$" ${BR2_CONFIG}; then
+        if [ ! -e ${TARGET_DIR}/init ]; then
+                install -m 0755 fs/cpio/init ${TARGET_DIR}/init;
+        fi
+
+	cp -a ${SKELETON_FS_DIR}/dev/console ${TARGET_DIR}/dev/console
+fi
+
+if grep -Eq "^BR2_INIT_SYSTEMD=y$" ${BR2_CONFIG}; then
+	# autologin service
+	cp ${SKELETON_FS_DIR}/lib/systemd/system/serial-getty@.service \
+	   ${TARGET_DIR}/lib/systemd/system/serial-getty@.service
+
+	ln -s /lib/systemd/system/serial-getty@.service \
+	     ${TARGET_DIR}/etc/systemd/system/getty.target.wants/serial-getty@ttyS2.service
+
+	# remove prompt login services
+	rm ${TARGET_DIR}/lib/systemd/system/console-getty.service
+	rm ${TARGET_DIR}/usr/lib/systemd/system/container-getty@.service
+fi
