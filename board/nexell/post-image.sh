ed994a534550e37dc8151b48b5471f3c663dae9c
jhkim <jhkim@nexell.co.kr>
2020-04-13 13:48:58 +0900

    buildroot: nexell: support auto run adb daemon at boottime
    This patch support auto run adb daemon at boottime
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I0cbf7ace7192b028826626863bb2271a7b34890c

--- board/nexell/post-image.sh
+++ board/nexell/post-image.sh
@@ -6,10 +6,18 @@ set -e
 SKELETON_FS_DIR=board/nexell/fs
 
 if grep -Eq "^BR2_PACKAGE_ANDROID_TOOLS_ADBD=y$" ${BR2_CONFIG}; then
-	install -m 0755 $SKELETON_FS_DIR/usr/bin/start_adbd.sh \
-			${TARGET_DIR}/usr/bin/start_adbd.sh
-	install -m 0755 $SKELETON_FS_DIR/usr/bin/stop_adbd.sh \
-			${TARGET_DIR}/usr/bin/stop_adbd.sh
+	install -m 0755 $SKELETON_FS_DIR/usr/bin/start_adbd.sh ${TARGET_DIR}/usr/bin/start_adbd.sh
+	install -m 0755 $SKELETON_FS_DIR/usr/bin/stop_adbd.sh ${TARGET_DIR}/usr/bin/stop_adbd.sh
+	install -m 0755 $SKELETON_FS_DIR/etc/init.d/adbd.sh ${TARGET_DIR}/etc/init.d/adbd.sh
+
+	if grep -Eq "^BR2_INIT_SYSTEMD=y$" ${BR2_CONFIG}; then
+		for i in 0 1 2 3 4 5 6
+		do
+			mkdir -p ${TARGET_DIR}/etc/rc$i.d
+			link=S01adbd; [ $i -lt 2 ] && link=K01adbd;
+			ln -sf /etc/init.d/adbd.sh ${TARGET_DIR}/etc/rc$i.d/$link
+		done
+	fi
 fi
 
 if grep -Eq "^BR2_PACKAGE_TSLIB=y$" ${BR2_CONFIG}; then
@@ -21,11 +29,9 @@ if grep -Eq "^BR2_PACKAGE_DIRECTFB=y$" ${BR2_CONFIG}; then
 fi
 
 if grep -Eq "^BR2_PACKAGE_MEMTESTER=y$" ${BR2_CONFIG}; then
-	install -m 0755 $SKELETON_FS_DIR/usr/bin/memtest.sh \
-			${TARGET_DIR}/usr/bin/memtest.sh
+	install -m 0755 $SKELETON_FS_DIR/usr/bin/memtest.sh ${TARGET_DIR}/usr/bin/memtest.sh
 fi
 
 if grep -Eq "^BR2_PACKAGE_LMBENCH=y$" ${BR2_CONFIG}; then
-	install -m 0755 $SKELETON_FS_DIR/usr/bin/bw_mem.sh \
-			${TARGET_DIR}/usr/bin/bw_mem.sh
+	install -m 0755 $SKELETON_FS_DIR/usr/bin/bw_mem.sh ${TARGET_DIR}/usr/bin/bw_mem.sh
 fi
